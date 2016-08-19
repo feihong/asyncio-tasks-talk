c31382d7aae026b1f2f030a7e91a66395ba9bff4
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-18 19:26:50 -0500

    Add async_task_v1 example

---
+++ async_task_v1/app.py
@@ -0,0 +1,41 @@
+"""
+To run:
+
+muffin app run
+
+"""
+import json
+import asyncio
+import muffin
+from muffin_playground import Application, WebSocketHandler, WebSocketWriter
+
+
+app = Application()
+app.register_static_resource()
+
+
+@app.register('/websocket/')
+async def websocket(request):
+    ws = muffin.WebSocketResponse()
+    await ws.prepare(request)
+
+    writer = WebSocketWriter(ws)
+
+    async for msg in ws:
+        print(msg.data)
+        if msg.data == 'start':
+            coroutine = long_task(WebSocketWriter(ws))
+            task = asyncio.ensure_future(coroutine)
+            await task
+            print('Finished task')
+            # writer.write(type='done')
+
+    return ws
+
+
+async def long_task(writer):
+    total = 100
+    for i in range(1, total+1):
+        writer.write(type='progress', value=i, total=total)
+        print(i)
+        await asyncio.sleep(0.05)
