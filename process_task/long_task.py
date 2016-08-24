2df3de932141e2f13b99642ab747bdeb9953cd91
Feihong Hsu <feihong.hsu@gmail.com>
2016-08-23 19:19:50 -0500

    Use the websocket-client synchronous library

--- process_task/long_task.py
+++ process_task/long_task.py
@@ -1,18 +1,22 @@
 import sys
 import time
+import json
 
-from websocketsync import WebSocketClient
+import websocket
 
 
 def long_task(url):
-    with WebSocketClient(url) as client:
-        total = 15
-        for i in range(1, total+1):
-            print(i)
-            # import ipdb; ipdb.set_trace()
-            client.write(type='progress', value=i, total=total)
-            time.sleep(0.05)
+    ws = websocket.WebSocket()
+    ws.connect(url)
 
+    total = 150
+    for i in range(1, total+1):
+        print(i)
+        data = dict(type='progress', value=i, total=total)
+        ws.send(json.dumps(data))
+        time.sleep(0.05)
+
+    ws.close()
 
 
 if __name__ == '__main__':
