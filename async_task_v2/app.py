eeb4c80073d966cc0d15db589811e59e6ff902d5
Feihong <feihong.hsu@gmail.com>
2016-09-07 11:31:55 -0500

    Refactor the async_task_v2 code

--- async_task_v2/app.py
+++ async_task_v2/app.py
@@ -11,19 +11,19 @@ app.register_special_static_route()
 class WSHandler(WebSocketHandler):
     async def on_open(self):
         self.task = None
+        self.writer = WebSocketWriter(self.websocket)
+
+    def task_done_callback(self, future):
+        self.task = None
 
     async def on_message(self, msg):
         print(msg)
         if msg.data == 'start' and not self.task:
-            coroutine = long_task(WebSocketWriter(self.websocket))
+            coroutine = long_task(self.writer)
             self.task = asyncio.ensure_future(coroutine)
-            self.task.add_done_callback(self.done_callback)
+            self.task.add_done_callback(self.task_done_callback)
         elif msg.data == 'stop' and self.task:
             self.task.cancel()
-            self.task = None
-
-    def done_callback(self, future):
-        self.task = None
 
 
 async def long_task(writer):
