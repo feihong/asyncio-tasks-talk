aac1a450c5c927f48a5760ef87735b70aaa12d6d
Feihong Hsu <feihong.hsu@gmail.com>
2016-09-07 14:12:22 -0500

    Refactor sync_task/app.py to use AbstractEventLoop.run_in_executor()

--- sync_task/app.py
+++ sync_task/app.py
@@ -6,33 +6,33 @@ from muffin_playground import Application, WebSocketHandler, ThreadSafeWebSocket
 
 
 app = Application()
-app.app.register_special_static_route()
+app.register_special_static_route()
 
 
 @app.register('/websocket/')
 class WSHandler(WebSocketHandler):
     async def on_open(self):
-        self.task = None
+        self.future = None
+        self.writer = ThreadSafeWebSocketWriter(self.websocket)
         self.stop_event = threading.Event()
 
     async def on_message(self, msg):
         print(msg)
-        if msg.data == 'start' and not self.task:
-            self.task = app.start_task_in_executor(
-                long_task,
-                ThreadSafeWebSocketWriter(self.websocket),
-                self.stop_event)
-            self.task.add_done_callback(self.done_callback)
-        elif msg.data == 'stop' and self.task:
+        if msg.data == 'start' and not self.future:
+            loop = asyncio.get_event_loop()
+            self.future = loop.run_in_executor(
+                None, long_computation, self.writer, self.stop_event)
+            self.future.add_done_callback(self.future_done_callback)
+        elif msg.data == 'stop' and self.future:
             self.stop_event.set()
 
-    def done_callback(self, future):
+    def future_done_callback(self, future):
         print('done!')
-        self.task = None
+        self.future = None
         self.stop_event.clear()
 
 
-def long_task(writer, stop_event):
+def long_computation(writer, stop_event):
     total = 150
     for i in range(1, total+1):
         if stop_event.is_set():
