e3add318c3135606629d4cc6f3380ed554c3c2ec
Feihong Hsu <feihong.hsu@gmail.com>
2016-09-08 00:21:53 -0500

    Completely change websocket task to make it more like the previous examples

--- websocket_task/app.py
+++ websocket_task/app.py
@@ -6,41 +6,19 @@ from muffin_playground import Application, WebSocketWriter
 
 app = Application()
 app.register_special_static_route()
-PAGE_SIZE = 6
-
-
-@app.register('/')
-async def index(request):
-    results = await fetch(0)
-    return app.render('index.plim', results=results, page_size=PAGE_SIZE)
 
 
 @app.register('/websocket/')
 async def websocket(request):
     ws = muffin.WebSocketResponse()
     await ws.prepare(request)
-    print('Websocket opened')
-
     writer = WebSocketWriter(ws)
 
-    page = 1
-    while not ws.closed:
-        results = await fetch(page)
-        if results is None:
-            break
-        writer.write(type='results', value=results, page=page)
-        page += 1
+    total = 150
+    for i in range(1, total+1):
+        writer.write(type='progress', value=i, total=total)
+        print(i)
+        await asyncio.sleep(0.05)
 
     await ws.close()
-    print('Websocket closed')
-
     return ws
-
-
-async def fetch(page):
-    if page >= 10:
-        return None
-    start = page * PAGE_SIZE + 1
-    end = start + PAGE_SIZE
-    await asyncio.sleep(1)
-    return [i for i in range(start, end)]
