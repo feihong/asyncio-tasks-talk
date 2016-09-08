8e30a273e229cd0faa7630fe894909085e85d43c
Feihong <feihong.hsu@gmail.com>
2016-09-07 23:10:03 -0500

    Refactor sync_task/app.py for clarity

--- sync_task/app.py
+++ sync_task/app.py
@@ -16,23 +16,23 @@ class WSHandler(WebSocketHandler):
         self.writer = ThreadSafeWebSocketWriter(self.websocket)
         self.stop_event = threading.Event()
 
-    async def on_message(self, msg):
-        print(msg)
-        if msg.data == 'start' and not self.future:
-            loop = asyncio.get_event_loop()
-            self.future = loop.run_in_executor(
-                None, long_computation, self.writer, self.stop_event)
-            self.future.add_done_callback(self.future_done_callback)
-        elif msg.data == 'stop' and self.future:
-            self.stop_event.set()
-
     def future_done_callback(self, future):
         print('done!')
         self.future = None
         self.stop_event.clear()
 
+    async def on_message(self, msg):
+        print(msg)
+        if msg.data == 'start' and not self.future:
+            loop = asyncio.get_event_loop()
+            self.future = loop.run_in_executor(
+                None, long_task, self.writer, self.stop_event)
+            self.future.add_done_callback(self.future_done_callback)
+        elif msg.data == 'stop' and self.future:
+            self.stop_event.set()
 
-def long_computation(writer, stop_event):
+
+def long_task(writer, stop_event: threading.Event):
     total = 150
     for i in range(1, total+1):
         if stop_event.is_set():
